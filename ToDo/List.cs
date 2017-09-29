d10b1fc3b429815e9d904a25cfa0e5f11961df09
Katona Dorottya <moodytea@yahoo.com>
2017-09-29 14:20:55 +0200

    add, remove and some error handling done

--- ToDo/List.cs
+++ ToDo/List.cs
@@ -1,10 +1,87 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
 namespace ToDo
 {
-    public class List
+    public class MyList
     {
-        public List()
+        public static List<string> listAsIs = new List<string>(){};
+        public static string[] stringToConvert = File.ReadAllLines("..//..//todolist.txt");
+
+
+        public static List<string> ListOfTasks()
         {
+            try
+            {
+				foreach (string s in stringToConvert)
+				{
+					listAsIs.Add(s);
+				}
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("conversion error:" + ex);
+            }
+
+            return listAsIs;
+
         }
+
+		public void TaskLister()
+		{
+            ListOfTasks();
+            if (listAsIs.Count.Equals(0))
+			{
+				Console.WriteLine("No 2Dos for today");
+			}
+            else
+            {
+				int i = 1;
+                foreach (string todos in listAsIs)
+				{ 
+					Console.WriteLine( (i) + " - " + todos);
+					i++;
+				}
+            }
+		}
+
+        public List<string> TaskAdder(string addedTask)
+		{
+            ListOfTasks();
+            listAsIs.Add(addedTask);
+            if (addedTask.Length.Equals(0))
+            {
+                Console.WriteLine("Unable to add: no task provided");
+            }
+            File.AppendAllText("..//..//todolist.txt", "\n" + addedTask);
+            return listAsIs;
+		}
+
+		public List<string> TaskRemover(int taskNumber)
+		{
+            ListOfTasks();
+
+            listAsIs.RemoveAt(taskNumber - 1);
+
+			File.Delete("..//..//todolist.txt");
+            int i = 1;
+            foreach (string todos in listAsIs)
+			{
+                if (i == 1)
+                {
+                    File.AppendAllText("..//..//todolist.txt", todos);
+                    i--;
+                }
+                else
+				File.AppendAllText("..//..//todolist.txt", "\n" + todos);
+			}
+
+            return listAsIs;
+			
+		}
+
+
+       
     }
 }
---
+++ ToDo/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Linq;
+
+namespace ToDo
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            //args = new string[]{"-r"};
+            MyList myList = new MyList();
+            if (args.Length.Equals(0))
+            {
+				Console.WriteLine("Command Line Todo application");
+				Console.WriteLine("=============================");
+				Console.WriteLine("");
+				Console.WriteLine("Command line arguments:");
+				Console.WriteLine("-l   Lists all the tasks");
+				Console.WriteLine("-a   Adds a new task");
+				Console.WriteLine("-r   Removes an task");
+				Console.WriteLine("-c   Completes an task");
+            }
+            else if (args.Contains("-l"))
+            {
+                myList.TaskLister();
+            }
+			else if (args.Contains("-a"))
+            {
+                myList.TaskAdder(args[1]);
+            }
+            else if (args.Contains("-r"))
+            {
+                int num = -1;
+
+                if (args.Count()==1)
+                {
+                    Console.WriteLine("Unable to remove: no index provided");
+                }
+                else if (!Int32.TryParse(args[1], out num))
+				{
+					Console.WriteLine("Unable to remove: index is not a number");
+				}
+                else if (args[1].Equals(""))
+				{
+					Console.WriteLine("Unable to remove: no index provided");
+				} 
+				else if (num > (MyList.listAsIs.Count) || num < 0)
+				{
+					Console.WriteLine("Unable to remove: index out of range");
+				}
+                else
+                {
+                    myList.TaskRemover(num);
+				}
+
+            }
+               
+                  
+        }
+    }
+}
